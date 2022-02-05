8444d8f604761468c83700e98ca44789c6da2b57
muizogundiran <muizakanji@gmail.com>
2022-02-05 16:08:45 +0100

    chore: let start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return (
-     <div>
-         Hello World
-     </div>
-  );
+  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 };
 
 export default App;
