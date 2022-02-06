3f7763390c4cba881ba7917f0c6b3fa7d39c5829
muizogundiran <muizakanji@gmail.com>
2022-02-06 13:35:11 +0100

    fix: tailwind colors not working due to versioning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      "theme-100": "#F2727D",
+      "theme-200": "#A63F5A",
+      "theme-300": "#181626",
+      "theme-400": "#F29C50",
+      "theme-500": "#D98859",
+    },
   },
   variants: {
     extend: {},
