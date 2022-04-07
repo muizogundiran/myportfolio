1e86e3c6d3aec9ca00980bbecbf8b80297abd661
muizogundiran <muizakanji@gmail.com>
2022-04-07 05:54:48 +0100

    fix: tailwind classes not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     colors: {
       transparent: "transparent",
